f9aab886b1142d216c43a9ea50006cbb45d20609
Leonardo <leonardoguerreirojoao@gmail.com>
2019-03-20 16:32:15 +0000

    add

--- chalenge6/chalenge6.js
+++ chalenge6/chalenge6.js
@@ -1,155 +1,154 @@
 var movementDescription;
 var movementValue;
 
-var expensesDescription = [];
-var expensesValue = [];
-
-var receiptsDescription = [];
-var receiptsValue = [];
-
 var movementList = document.getElementById("movementlist");
 var deleteList = document.getElementById("deleteList");
 
-var expensesSum = 0;
-var receiptSum = 0;
+let transactionNumber = 0;
 
-/** saves the values put in -- checks if it was positive or negative -- alerts if there are incorrect inputs*/
+/** Info inside of Object --> description, amount, type, use */
+var transactionData = {};
+
+/** saves the values put in 
+ *  checks if it was positive or negative 
+ *  alerts if there are incorrect inputs
+ *  displays totalBalance and values*/
 function onSubmit()
 {
+    transactionNumber++;
     /** Form inputs and their vairables */
-    var transferValue = document.getElementById("transfervalue").value;
-    var transfertype = document.getElementById("transfertype").value;
-    var result;
+    /** Description */movementDescription = document.getElementById("description").value;
+    /** Amount */movementValue = document.getElementById("moneyTransfer").value > 0 ? document.getElementById("moneyTransfer").value : false;
+    /** Type */var transferType = document.getElementById("transfervalue").value;
+    /** Use */var transfertUse = document.getElementById("transfertype").value;
 
-    movementDescription = document.getElementById("description").value;
-    movementValue = document.getElementById("moneyTransfer").value > 0 ? document.getElementById("moneyTransfer").value : false;
+    var transactionID = "transactionNumber_" + transactionNumber;
 
-    /** innerHTML Values */
-    var transferName;
-    var moneyValue;
-    var moneyuse;
+    if(transferType == "expenses"){
+        movementValue = -movementValue;
+    }
+
+    /** adds new information to Object --> nested Object */
+    transactionData[transactionID] = {
+        description: movementDescription,
+        value: movementValue,
+        type: transferType,
+        use: transfertUse
+    }
+
+    console.log(transactionData);
+
+    var movValNumber = parseFloat(movementValue);
 
     /** class Values */
     var iconColor = '';
     var iconType = '';
     var textColor = '';
 
+    //create new list element
+    var divListElement = document.createElement('div');
+
+    //** general element atributes */
+    divListElement.setAttribute("class", transactionID);
+    divListElement.setAttribute("id", transactionID);
+
     if (movementValue != false)
     {
-        if (transferValue == "expenses")
+        if (transferType == "expenses")
         {
-            console.log('AAA');
-            expensesDescription.push(movementDescription);
-            expensesValue.push(movementValue);
-
             iconColor = 'minusicon';
             iconType = 'fa-minus-circle';
             textColor = 'negative';
 
             document.getElementById("trackingform").reset();
 
-            transferName = movementDescription;
-            moneyValue = movementValue;
 
-            result = expensesValue.map(Number);
-
-            expensesSum = result.reduce(add);
-            console.log("exp" + expensesSum);
-            console.log("1 Name" + transferName);
-            console.log("1 Value" + moneyValue);
+            console.log('AAA');
+            console.log("1 Name-" + movementDescription);
+            console.log("1 Value-" + movementValue);
         }
-        else if (transferValue == "receipts")
+        else if (transferType == "receipts")
         {
-            console.log('BBB');
-            receiptsDescription.push(movementDescription);
-            receiptsValue.push(movementValue);
-
             iconColor = 'plusicon';
             iconType = 'fa-plus-circle';
             textColor = 'positive';
 
             document.getElementById("trackingform").reset();
 
-            transferName = movementDescription;
-            moneyValue = movementValue;
-
-            result = receiptsValue.map(Number);
-
-            receiptSum = result.reduce(add);
-            console.log("rec" + receiptSum);
-            console.log("2 Name" + transferName);
-            console.log("2 Value" + moneyValue);
+            console.log('BBB');
+            console.log("2 Name-" + movementDescription);
+            console.log("2 Value-" + movementValue);
         }
         else
         {
             alert('Please select if it\'s an expense or a receipt.');
+            return null;
         }
     } else
     {
         alert('Only positive numbers');
+        return null;
     }
-    getFinalResult(receiptSum, expensesSum);
 
-    console.log("3 Name" + transferName);
-    console.log("3 Value" + moneyValue);
+    /** display totalBalance */
+    console.log("num" + transactionNumber + " -- type" + transferType);
+    var totalValue = getTotalBalance(transactionNumber);
+    document.getElementById("totalbalance").innerHTML = totalValue.toFixed(2);
 
-    if (transferName !== undefined && moneyValue !== undefined)
+    /** remove minus sign from displayed number */
+    if(movValNumber<0){
+        movValNumber = -movValNumber
+    }
+
+    if (movementDescription !== undefined && movementValue !== undefined)
     {
-        moneyuse = transfertype;
-
         /** gets current date */
         var currentDay = new Date;
-        var date = currentDay.getDate() + '/' + (currentDay.getMonth()+1)+ '/' + currentDay.getFullYear();
+        var date = currentDay.getDate() + '/' + (currentDay.getMonth() + 1) + '/' + currentDay.getFullYear();
         var time = currentDay.getHours() + ':' + currentDay.getMinutes() + ':' + currentDay.getSeconds();
-        
+
         /** list element template */
         var movementListElement = '<div class="row justify-content-between transactionslist ">' +
-        '<div class="col-1 icon">' +
-        '<span class="fa-stack fa-xs ' + iconColor + ' ">' +
-        '<i class="fas fa-circle fa-stack-2x"></i>' +
-        '<i class="fas ' + iconType + ' fa-stack-1x fa-inverse fa-lg"></i>' +
-        '</span>' +
-        '</div>' +
-        '<div class="col-11">' +
-        '<div class="row justify-content-between transfer">' +
-        '<div class="col-9 transfername">' + transferName + '</div>' +
-        '<div class="col-3 text-right moneyvalue ' + textColor + '">€' + moneyValue + '</div>' +
-        '</div>' +
-        '<div class="row justify-content-between transferdescription">' +
-        '<div class="col-4 date">' + date + '</div>' +
-        '<div class="col-4 time">' + time + '</div>' +
-        '<div class="col-4 text-right moneyuse">' + transfertype + '</div>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
-        
-        //create new list element
-        var divListElement = document.createElement('div');
+            '<div class="col-1 icon">' +
+            '<span class="fa-stack fa-xs ' + iconColor + ' ">' +
+            '<i class="fas fa-circle fa-stack-2x"></i>' +
+            '<i class="fas ' + iconType + ' fa-stack-1x fa-inverse fa-lg"></i>' +
+            '</span>' +
+            '</div>' +
+            '<div class="col-11">' +
+            '<div class="row justify-content-between transfer">' +
+            '<div class="col-9 transfername">' + movementDescription + '</div>' +
+            '<div class="col-3 text-right moneyvalue ' + textColor + '">€' + movValNumber.toFixed(2) + '</div>' +
+            '</div>' +
+            '<div class="row justify-content-between transferdescription">' +
+            '<div class="col-4 date">' + date + '</div>' +
+            '<div class="col-4 time">' + time + '</div>' +
+            '<div class="col-4 text-right moneyuse">' + transfertUse + '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
 
-        divListElement.setAttribute("class",transferName);
-        divListElement.setAttribute("value", moneyValue);
 
         divListElement.innerHTML = movementListElement;
-
         movementList.appendChild(divListElement);
 
-        //add description(transferName) to deleteList
+        //add description(movementDescription) to deleteList
         var optDeleteList = document.createElement('option');
-        optDeleteList.setAttribute("class",transferName);
+        optDeleteList.setAttribute("class", transactionID);
 
-        optDeleteList.value = transferName;
-        optDeleteList.innerHTML = transferName;
+        optDeleteList.innerHTML = movementDescription;
 
         deleteList.appendChild(optDeleteList);
-
-    } else{
+    } else
+    {
         console.log('put in values');
     }
 }
 
-function addTransferTypeOption(){
+/** adds new TransferType getting input */
+function addTransferTypeOption()
+{
     var newTransferType = document.getElementById("new-transfertype").value;
-
     var optTransferType = document.createElement('option');
 
     optTransferType.value = newTransferType;
@@ -159,44 +158,58 @@ function addTransferTypeOption(){
     document.getElementById("newstuffform").reset();
 }
 
-function deleteListMember(){
-    var deleteListItem = document.getElementById("deleteList").value;
-    // var deleteListItemValue = document.getElementById(transferName).value;
+/** Deletes selected member */
+function deleteListMember()
+{
+    var deleteListItem = document.getElementById("deleteList");
+    var selectedItem = deleteListItem.options[deleteListItem.selectedIndex].classList;
 
-    console.log(deleteListItem);
-    var selectedItems = document.getElementsByClassName(deleteListItem);
-    while(selectedItems.length > 0){
+    console.log(selectedItem);
+    console.log(document.getElementById("deleteList").classList);
+    console.log("delete-.-.-.-.-" + deleteListItem);
+
+    var selectedItems = document.getElementsByClassName(selectedItem[0]);
+    var selectedTransaction = selectedItem[0].slice(-1);
+
+    // deletes object
+    delete transactionData[selectedItem];
+
+    console.log("selectedTransaction" + selectedTransaction);
+
+    var totalValue = getTotalBalance(transactionNumber);
+    document.getElementById("totalbalance").innerHTML = totalValue.toFixed(2);
+
+    console.log(transactionData);
+
+    //deletes List Opt and list div
+    while (selectedItems[0])
+    {
         selectedItems[0].parentNode.removeChild(selectedItems[0]);
     }
-    console.log(receiptsValue);
-
-    // var index = receiptsValue.indexOf(deleteListItemValue);
-    //     if (index > -1) {
-    //         receiptsValue.splice(index, 1);
-    // }
-
-    console.log(expensesValue);
-    
-    getFinalResult(receiptSum, expensesSum);
 }
 
-/** adds the values in the arrays using reduce(add)*/
-function add(added, a)
+/** Calculation of the totalBalance -- gets all values from all nested objects and adds them */
+function getTotalBalance(transactionNumber)
 {
-    return added + a;
-}
+    var transaction;
+    var totalValue = 0;
+    var thisNumber;
 
-/** adds the finalResult */
-function getFinalResult(receipt, expense)
-{
-    var finalResult = receipt - expense;
+    for (var i = 1; i <= transactionNumber; i++)
+    {
+        transaction = "transactionNumber_" + i;
+        console.log(i + "-----" + transaction);
+        console.log(i + "-----" + transactionData);
 
-    console.log("2" + finalResult);
-
-    /** toFixed limits decimal houses to 2 */
-    document.getElementById("totalbalance").innerHTML = finalResult.toFixed(2);
-
-    console.log("3" + finalResult);
-
-    return finalResult;
+        /** Cheks if total is not undefined, so it is 0 instead*/
+        if (transactionData[transaction] != undefined)
+        {
+            thisNumber = parseFloat(transactionData[transaction].value);
+        } else
+        {
+            thisNumber = 0;
+        }
+        totalValue += thisNumber;
+    }
+    return totalValue;
 }
